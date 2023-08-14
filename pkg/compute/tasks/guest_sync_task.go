b972a9d422af3290b433b1024d2bf0803014fc15
tianxiang chen <1134318388@qq.com>
2023-08-14 20:35:35 +0800

    test-log-6

--- pkg/compute/tasks/guest_sync_task.go
+++ pkg/compute/tasks/guest_sync_task.go
@@ -53,9 +53,8 @@ func (self *GuestSyncConfTask) OnSyncComplete(ctx context.Context, obj db.IStand
 	guest := obj.(*models.SGuest)
 
 	//添加日志 log test
-	notesTest := jsonutils.NewDict()
-	notesTest.Add(jsonutils.NewString(self.Params.String()), "Params")
-	logclient.AddActionLogWithContext(ctx, self, logclient.ACT_ADDTAG, notesTest, self.UserCred, false)
+	logclient.AddActionLogWithStartable(self, guest, logclient.ACT_VM_SYNC_CONF, data, self.UserCred, false)
+	logclient.AddActionLogWithStartable(self, guest, logclient.ACT_VM_SYNC_CONF, self.Params, self.UserCred, false)
 
 	if fwOnly, _ := self.GetParams().Bool("fw_only"); fwOnly {
 		db.OpsLog.LogEvent(guest, db.ACT_SYNC_CONF, nil, self.UserCred)
