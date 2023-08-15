fd69f6b136841c87921d84a8bc88404b18d361a8
tianxiang chen <1134318388@qq.com>
2023-08-15 12:14:20 +0800

    ifname-8

--- pkg/compute/models/guest_actions.go
+++ pkg/compute/models/guest_actions.go
@@ -2427,13 +2427,12 @@ func (self *SGuest) PerformChangeIpaddr(ctx context.Context, userCred mcclient.T
 		} `json:"statistics"`
 	}
 
-	var parsedData struct {
-		IfnameDetails []IfnameDetailStruct `json:"ifnameDetails"`
-	}
+	type parsedDataStruct []*IfnameDetailStruct
+	parsedData := parsedDataStruct{}
 	err = ifnameDetail.Unmarshal(&parsedData)
 	notesTest.Add(jsonutils.NewString(err.Error()), "err")
 
-	for _, detail := range parsedData.IfnameDetails {
+	for _, detail := range parsedData {
 		if detail.HardwareAddress == needMacAddr {
 			fmt.Printf("MAC 地址 %s 对应的网卡名称是 %s\n", needMacAddr, detail.Name)
 			notesTest.Add(jsonutils.NewString(detail.Name), "detail.Name")
